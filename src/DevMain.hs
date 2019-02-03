00c11b213525519cce67b7a6fc56047076b64df8
kayhide <kayhide@gmail.com>
2019-02-03 19:14:46 +0900

    watch redis queue.

--- src/DevMain.hs
+++ src/DevMain.hs
@@ -5,7 +5,7 @@ import           ClassyPrelude
 import qualified Data.Aeson           as Aeson
 import           Database.Persist
 import           Database.Persist.Sql
-import           Database.Redis
+import qualified Database.Redis       as Redis
 
 import           App.Config           (AppConfig (..))
 import           Configurable         (activate)
@@ -24,16 +24,30 @@ run = do
 
 type AppM a = ReaderT AppConfig IO a
 
+queue :: Text
+queue ="sidekiq_rb_development:queue:default"
+
 app :: AppM ()
 app = do
   listJobs
   listPredictors
+  forever $ do
+    Redis.run $ do
+      Redis.brpop [encodeUtf8 queue] 1 >>= \case
+        Left _ -> fail "Failed to read queue"
+        Right x' -> case x' of
+          Nothing -> print x'
+          Just (queue', x'') -> do
+            putStrLn $ "Fetched from queue: " <> decodeUtf8 queue'
+            case (Aeson.eitherDecode . fromStrict) x'' of
+              Left msg                           -> putStrLn . pack $ msg
+              Right (job :: JobWrapper TrainJob) -> print job
 
 
 listJobs :: AppM ()
 listJobs = do
   jobs :: [Either String (JobWrapper TrainJob)] <- Redis.run $ do
-    xs <- lrange "sidekiq_rb_development:queue:default" 0 (negate 1)
+    xs <- Redis.lrange (encodeUtf8 queue) 0 (negate 1)
     case xs of
       Left _ -> fail "Failed to read queue"
       Right xs' -> do
